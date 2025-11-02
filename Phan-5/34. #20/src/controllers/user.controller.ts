1f283d088da25e87447a08b196ea0af7e668e119
Pham Van Minh <pvminh23@clc.fitus.edu.vn>
2025-11-03 01:35:50 +0700

    Video 53. #39.

--- Phan-5/34. #20/src/controllers/user.controller.ts
+++ Phan-5/34. #20/src/controllers/user.controller.ts
@@ -8,4 +8,9 @@ const getCreateUserPage = (req: Request, res: Response) => {
     return res.render("create-user");   // Có hay không có .ejs cũng được
 }
 
-export { getHomePage, getCreateUserPage };
+const postCreateUser = (req: Request, res: Response) => {
+    console.log(">>> Check data: ", req.body);  // Chưa lấy đươch data
+    return res.redirect("/");   // Dùng res.redirect thì đường URL hiện trên trình duyệt mới đúng, res.render thì không đúng
+}
+
+export { getHomePage, getCreateUserPage, postCreateUser };
--- Phan-5/34. #20/src/routes/web.ts
+++ Phan-5/34. #20/src/routes/web.ts
@@ -1,12 +1,12 @@
 import express, {Express} from "express";       // Express là kiểu dữ liệu type script cho app
-import { getCreateUserPage, getHomePage } from "../controllers/user.controller";
+import { getCreateUserPage, getHomePage, postCreateUser } from "../controllers/user.controller";
 
 const router = express.Router();
 
 const webRoutes = (app: Express) => {
-  router.get("/", getHomePage);
-
+  router.get("/", getHomePage);   // Gọi hàm getHomePage mà không truyền gì hết thì nó sẽ truyền tất cả cho getHomePage ở user.controller trong đó có tham số req, res
   router.get("/create-user", getCreateUserPage);
+  router.post("/handle-create-user", postCreateUser);
 
   app.use("/", router);     // Website xuat phat tu duong link URL nay
 };
