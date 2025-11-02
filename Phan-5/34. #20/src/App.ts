535556b17777c691ae4fee0ac24b84f6be4d8978
Pham Van Minh <pvminh23@clc.fitus.edu.vn>
2025-11-02 13:01:51 +0700

    Video 44. #30

--- Phan-5/34. #20/src/App.ts
+++ Phan-5/34. #20/src/App.ts
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// const express = require('express')
+import express from "express";
+import 'dotenv/config'
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World abc!')
@@ -8,4 +11,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  console.log("env port: ", process.env.PORT)
 })
