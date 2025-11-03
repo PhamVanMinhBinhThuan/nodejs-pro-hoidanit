973efd0032cd51d15abc2b710c839260936fcd6b
Pham Van Minh <pvminh23@clc.fitus.edu.vn>
2025-11-03 15:28:30 +0700

    Video 62. #48.

--- Phan-5/34. #20/src/controllers/user.controller.ts
+++ Phan-5/34. #20/src/controllers/user.controller.ts
@@ -13,12 +13,14 @@ const getCreateUserPage = (req: Request, res: Response) => {
     return res.render("create-user");   // Có hay không có .ejs cũng được
 }
 
-const postCreateUser = (req: Request, res: Response) => {
+const postCreateUser = async (req: Request, res: Response) => {
     // Object Destructuring
     const { fullName, email, address } = req.body;
 
     // Handle Create User
-    handleCreateUser(fullName, email, address);
+    // Do hàm handleCreateUser định nghĩa ở service là async nên có await và async
+    // Lưu ý khi thao tác với database cần phải có await và async không thì sẽ bị lỗi
+    await handleCreateUser(fullName, email, address);
     
     return res.redirect("/");   // Dùng res.redirect thì đường URL hiện trên trình duyệt mới đúng, res.render thì không đúng
 }
--- Phan-5/34. #20/src/services/user.service.ts
+++ Phan-5/34. #20/src/services/user.service.ts
@@ -1,15 +1,22 @@
 import getConnection from "../config/database";
 
 // User.service sẽ phụ tá cho user.controller
-const handleCreateUser = (
+const handleCreateUser = async (
     fullName: string, 
     email: string, 
     address: string) => {
         // Insert into database
-
-        // Return result
-        console.log(">>> Insert a new user")
-
+        const connection = await getConnection();
+        try {
+            const sql = 'INSERT INTO `users`(`name`, `email`, `address`) VALUES (?, ?, ?)';
+            const values = [fullName, email, address];
+            const [result, fields] = await connection.execute(sql, values);
+            // console.log(result);    // Khong can cung dc
+            // return result;  // Khong can cung dc
+        } catch (err) {
+            console.log(err);
+            return [];
+        }
 }
 
 const getAllUsers = async() => {
